36ee16f69dd8edbe61b41b83fba8f7bc70abd466
Jonas Bolldén <jonas@bollden.se>
2019-03-26 09:43:12 +0100

    Added 'http://' to the parameter.

--- develop.sh
+++ develop.sh
@@ -5,7 +5,7 @@ echo -e "\n\033[0;32mStarting Hugo development server...\033[0m\n"
 myip=`curl https://ipinfo.io/ip`
 
 hugo server \
-  --baseURL=$myip \
+  --baseURL="http://$myip" \
   --bind=0.0.0.0 \
   --buildDrafts \
   --disableFastRender \
